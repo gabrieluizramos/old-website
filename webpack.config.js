00a11e8802291800fec3f1acad4e1a62cb966ce2
Gabriel Ramos <gabriel.l.ramos@walmart.com>
2017-09-01 15:06:06 -0300

    Create new structure of assets (now using webpack)

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+
+    // general config
+    entry: './assets/index.js',
+    output: {
+        filename: 'dist.js',
+        path: path.resolve(__dirname, 'assets/dist'),
+        publicPath: path.resolve(__dirname, 'assets/dist')
+    },
+
+    // DevServer config
+    devServer: {
+      publicPath: '/assets/dist/',
+      port: 8080
+    },
+
+    // rules config
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+
+    // plugins config
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+
+};
+
+console.log('====path is', path.resolve(__dirname, '/assets/dist/'))
+
+module.exports = config;
