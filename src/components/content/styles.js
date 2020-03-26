92a842a36e5b51d4c25211f7fed1f8eae6a25704
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2020-03-26 10:02:48 -0300

    chore(spacing): update paragraphs spacin

--- src/components/content/styles.js
+++ src/components/content/styles.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
 
-import { spacing } from '@gabrieluizramos/preferences/variables';
+import { spacing, font } from '@gabrieluizramos/preferences/variables';
 
 export const Content = styled.section`
-  max-width: 500px;
+  font-size: ${font.size.medium};
+  max-width: 550px;
   margin: 0 auto;
   position: relative;
   line-height: 2;
   padding: 0 ${spacing.double};
+
+  p:not(:last-of-type) {
+    margin-bottom: ${spacing.double};
+  }
 `;
